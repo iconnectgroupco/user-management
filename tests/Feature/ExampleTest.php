bffe3d76c7bf887ee67a0ccf09b3fedc707aa69a
Rupesh Singh <rupesh.singhghy@gmail.com>
2024-11-18 10:23:47 +0530

    workflow tests.yml updated

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,21 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        // Create a test user
+        $user = User::factory()->create();
 
+        // Log the user in
+        $response = $this->actingAs($user)->get('/');
+
+        // Assert that the status code is 200
         $response->assertStatus(200);
     }
 }
